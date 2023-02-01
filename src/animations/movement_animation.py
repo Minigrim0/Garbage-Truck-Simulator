bba7a2f4c4a6527f3f9d626566647f8b1d289b4d
Minigrim0 <grimauflorent@gmail.com>
2023-02-01 16:27:47 +0100

    [Upd] Movement animation has correct attribute types

--- src/animations/movement_animation.py
+++ src/animations/movement_animation.py
@@ -1,9 +1,10 @@
 from src.abstracts.animation.speed import AnimationSpeed
+from src.abstracts.animation.path import AnimationPath
 
 
 class MovementAnimation:
     """Combines a path and a speed to create an animation."""
 
-    def __init__(self, path: list, speed: int):
-        self.path = path
+    def __init__(self, path: AnimationPath = None, speed: AnimationSpeed = None):
+        self.path: AnimationPath = path
         self.speed: AnimationSpeed = speed
