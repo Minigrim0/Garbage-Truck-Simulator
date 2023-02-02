e97999ed1f052408b7118922f54b944b843d9b32
Minigrim0 <grimauflorent@gmail.com>
2023-02-02 12:55:37 +0100

    [Upd] Added health_bar to truc

--- src/truck.py
+++ src/truck.py
@@ -4,17 +4,32 @@ import pygame as pg
 
 # from src.utils.rot_center import rot_center
 from src.utils.bound import bound
+from UI.components.loading_bar import LoadingBar
+from src.animations.sprite_animation import SpriteAnimation
+
 
 class Truck(object):
+    NONE = 0
+    UP = 1
+    DOWN = 2
 
     def __init__(self):
         self.image: pg.Surface = None
-        self.Nitro = False
 
-        self.position = (15, 800)
+        self.nitro_level = 0
+        self.nitro = False
+        self.nitro_animation: SpriteAnimation = None
+
+        self.position = (15, 500)
         self.health = 100
+        self.max_health = 100
         self.speed = 30  # km/h
-        self.steering_speed = 10
+        self.steering_speed = 100  # px/s
+        self.steering = Truck.NONE
+
+        self.health_bar: LoadingBar = None
+
+        self.sounds: dict = {}
 
     @property
     def actual_speed(self):
@@ -24,12 +39,45 @@ class Truck(object):
         """
         return (self.speed / 3.6) * 64
 
+    @property
+    def alive(self):
+        return self.health > 0
+
     def load(self):
         logging.info("Loading truck")
         self.image = pg.image.load("assets/images/CamionPoubelle.png").convert_alpha()
 
+        self.health_bar = LoadingBar(
+            (0, 0),
+            (self.image.get_width(), 5),
+            max_val=self.max_health, initial_val=self.max_health,
+            animation_type="linear", speed=50
+        )
+
+        self.nitro_animation = SpriteAnimation(
+            "assets/images/animations/flames/", speed=15,
+            rotation=90, loop=-1
+        )
+        self.nitro_animation.play()
+
+        self.sounds["nitro"] = pg.mixer.Sound("assets/sounds/truck/nitro.ogg")
+
     def draw(self, screen):
+        self.health_bar.draw(screen, self.position)
         screen.blit(self.image, self.position)
+        self.nitro_animation.draw(screen, self.position)
+
+    def hurt(self, damage):
+        self.health -= damage
+
+    def heal(self, heal):
+        self.health += heal
+
+    def update(self, timeElapsed: float):
+        if self.steering != Truck.NONE:
+            self.move(self.steering == Truck.UP, timeElapsed)
+        self.health_bar.update(timeElapsed)
+        self.nitro_animation.update(timeElapsed)
 
     def move(self, up: bool, timeElapsed: float):
         if up:
@@ -44,13 +92,14 @@ class Truck(object):
             )
 
     def handleEvents(self, event):
+        self.steering = Truck.NONE
+
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_DOWN:
-                self.move(False, 1)
+                self.steering = Truck.DOWN
             elif event.key == pg.K_UP:
-                self.move(True, 1)
+                self.steering = Truck.UP
             elif event.key == pg.K_RIGHT:
-                if not Constants.Nitro and not Constants.CamionPart:
-                    Constants.Sounds["Nitro"].play()
-                    Constants.Nitro = True
-                    PuissNitroBas = Constants.PuissNitro
+                if not self.nitro:
+                    self.sounds["nitro"].play()
+                    self.nitro = True
